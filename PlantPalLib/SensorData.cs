75e1f5f2a10df0989825599b3bc3af26a5ce3f0e
miksezea <mikk289e@edu.zealand.dk>
2023-05-08 10:03:04 +0200

    Add project files.

---
+++ PlantPalLib/SensorData.cs
@@ -0,0 +1,18 @@
+﻿namespace PlantPalLib
+{
+    public class SensorData
+    {
+        public string? DateTime { get; set; }
+        public float PHValue { get; set; }
+        public int Humidity { get; set; }
+
+        public void ValidatePHValue()
+        {
+            throw new NotImplementedException();
+        }
+        public void ValidateHumidity()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
