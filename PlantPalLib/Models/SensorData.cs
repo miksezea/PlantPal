e23cc8bdcc838cc866a9eba23b7b48225d5d814b
miksezea <mikk289e@edu.zealand.dk>
2023-05-09 13:37:47 +0200

    Small change in sensordata

--- PlantPalLib/Models/SensorData.cs
+++ PlantPalLib/Models/SensorData.cs
@@ -28,7 +28,9 @@
         public void ValidateLightValue()
         {
             if (Light < 0)
+            {
                 throw new ArgumentOutOfRangeException();
+            }
         }
 
         public void Validate()
